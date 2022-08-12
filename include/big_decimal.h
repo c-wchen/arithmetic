241630ebfc54609813a7555c0b49cf0aefffcc50
wchen <48781700+chenwchen@users.noreply.github.com>
2022-08-13 00:54:51 +0800

    feat: 添加基本功能模块，未实现

---
+++ include/big_decimal.h
@@ -0,0 +1,11 @@
+//
+// Created by 忘尘 on 2022/8/13.
+//
+
+#ifndef ARITHMETIC_BIG_DECIMAL_H
+#define ARITHMETIC_BIG_DECIMAL_H
+void AddDecimal(char *first, char *second, char *result);
+void SubDecimal(char *first, char *second, char *result);
+void MulDecimal(char *first, char *second, char *result);
+void DivDecimal(char *first, char *second, char *result);
+#endif //ARITHMETIC_BIG_DECIMAL_H

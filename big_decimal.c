2fd9cfef6c7306cd4cc8fa91b49f4e5a28b159f0
wchen <48781700+chenwchen@users.noreply.github.com>
2022-08-13 00:54:51 +0800

    feat: 添加基本功能模块，未实现

---
+++ big_decimal.c
@@ -0,0 +1,19 @@
+//
+// Created by 忘尘 on 2022/8/13.
+//
+
+#include "big_decimal.h"
+
+
+void AddDecimal(char *first, char *second, char *result) {
+
+}
+void SubDecimal(char *first, char *second, char *result) {
+
+}
+void MulDecimal(char *first, char *second, char *result) {
+
+}
+void DivDecimal(char *first, char *second, char *result) {
+
+}

d51bc22fc9d117505df8eb62e05f2802fe433cc4
wchen <48781700+chenwchen@users.noreply.github.com>
2022-08-14 16:13:12 +0800

    feat: 添加小数四舍五入保留位数

--- include/big_decimal.h
+++ include/big_decimal.h
@@ -4,6 +4,8 @@
 
 #ifndef ARITHMETIC_BIG_DECIMAL_H
 #define ARITHMETIC_BIG_DECIMAL_H
+#define REVERSED_BITS 7
+#define REVERSED_INTERVAL 5
 void AddDecimal(char *first, char *second, char *result);
 void SubDecimal(char *first, char *second, char *result);
 void MulDecimal(char *first, char *second, char *result);
